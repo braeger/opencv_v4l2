24bf9f258a3c4ab3f280e630dbad24ec7af8976b
Steve132 <steve@soapforge.com>
2022-01-10 06:08:23 -0500

    Update CMakeLists.txt and v4l2helper_webcam.cpp

--- example/v4l2helper_webcam.cpp
+++ example/v4l2helper_webcam.cpp
@@ -37,17 +37,7 @@ void yuyv2yuva8(unsigned char* yuva8,const unsigned char* yuyv,size_t N)
 		out_pair[7]=0xFF;
 	}
 }
-void yuyv2y(unsigned char* y,const unsigned char* yuyv,size_t N)
-{
-	N/=4; //every 4 bytes, load 2 pixels.
-	for(size_t i=0;i<N;i++)
-	{
-		const unsigned char* in_pair=yuyv+4*i;
-		unsigned char* out_pair=y+2*i; //TODO probably implement this with words swaps and shifts but meh. portability.
-		out_pair[0]=in_pair[0]; //y0
-		out_pair[1]=in_pair[2]; //y1
-	}
-}
+
 
 int main(int argc, char **argv)
 {
@@ -55,10 +45,10 @@ int main(int argc, char **argv)
 	unsigned int pix_format = V4L2_PIX_FMT_YUYV; // Or: V4L2_PIX_FMT_MJPEG.  see all available with: v4l2-ctl --list-formats-ext
 	// TODO: YUYV conversion for display issue: let's just write as an image file just to confirm.  
 	// But for live display adopt to v4l2_helper from : https://gist.github.com/MacDue/36199c3f3ca04bd9fd40a1bc2067ef72
-	size_t width = 1280, height = 720; // default for YUYV;
+	size_t width = 640, height = 480; // default for YUYV;
 	enum io_method IO_method = IO_METHOD_MMAP; // Also: IO_METHOD_USERPTR and IO_METHOD_MMAP
 
-	CImg<unsigned char> visu(width,height,1,4,0);
+	CImg<unsigned char> visu(width,height,1,4);
 	std::vector<unsigned char> out_buf(width*height*4);
 	CImgDisplay main_disp(visu,"preview");
 
@@ -74,7 +64,7 @@ int main(int argc, char **argv)
 	main_disp.show();
 	while(!main_disp.is_closed())
 	{
-		main_disp.wait(33);
+		main_disp.wait(10);
 		int bytes_used;
 		v4l2helper_frame_t* frame;
 		if (v4l2helper_cam_get_frame(cam,&frame) < 0)
@@ -82,7 +72,7 @@ int main(int argc, char **argv)
 			break;
 		}
 		v4l2helper_frame_get_data(frame,&ptr_data,&bytes_used);
-		cout << "BYTES USED: " << bytes_used << std::endl;
+		//cout << "BYTES USED: " << bytes_used << std::endl;
 
 		//every 4 bytes is 2 pixels. <--> width*height*(32 bpp_out/16 bpp_in) <-> 2
 		if(!ptr_data || bytes_used*2 != out_buf.size())
@@ -91,11 +81,13 @@ int main(int argc, char **argv)
 		}
 
 		//yuyv2yuva8(out_buf.data(),ptr_data,bytes_used);
-		yuyv2y(out_buf.data(),ptr_data,bytes_used);
+		yuyv2yuva8(out_buf.data(),ptr_data,bytes_used);
 
 		//https://www.codefull.net/2014/11/cimg-does-not-store-pixels-in-the-interleaved-format/
-		visu.assign(out_buf.data(), 1, width, height, 1, true);
+		visu.assign(out_buf.data(), 4, width, height, 1);
 		visu.permute_axes("yzcx");
+		visu.channels(0,2);//YUVA->YUV
+		visu.YCbCrtoRGB();
 
 //		visu.save("frame.jpg");
 		main_disp.display(visu);
